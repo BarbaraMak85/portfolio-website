64d083e84f06d3fc52412a074e731c50785058b1
Barbara <barbara.makowiec@wp.pl>
2022-03-13 08:52:12 +0100

    CookieInfo

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { About, Header, Skills, Testimonial, Work, Footer } from "./containter";
 import { Navbar } from "./components";
+import { CookiesProvider } from "react-cookie";
 import "./App.scss";
-
+import CookieInfo from "./components/CookieInfo/CookieInfo";
 function App() {
   return (
     <div className="app">
-      <Navbar />
-      <Header />
-      <About />
-      <Work />
-      <Skills />
-      <Testimonial />
-      <Footer />
+      <CookiesProvider>
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonial />
+        <Footer />
+        <CookieInfo />
+      </CookiesProvider>
     </div>
   );
 }
